1c28096f8d122e540630e61b8d9647b7e2feccbc
Thomas Bonemeijer Desktop <thomas.bonemeijer@student.ehb.be>
2019-07-09 00:49:33 +0200

    Added health system and HealthPlant mechanict (basic start)

---
+++ MyGame/Assets/Scripts/HealthPlantHandler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPlantHandler : MonoBehaviour
+{
+    private GameObject player;
+    private float playerHealth;
+    public GameObject fruit;
+    public float resetFruitTime = 5f;
+    private Vector3 fruitScale0 = new Vector3(0f, 0f, 1f);
+    private Vector3 fruitScale1 = new Vector3(1f, 1f, 1f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerHealth = player.GetComponent<PlayerHandler>().health;
+    }
+
+     void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name == "Player" && playerHealth < 100f)
+    {
+        fruit.transform.localScale = fruitScale0;
+        gameObject.GetComponent<CircleCollider2D>().enabled = false;
+        player.GetComponent<PlayerHandler>().health = 100f;
+        Invoke("ResetFruit", resetFruitTime);
+    }
+    }
+
+    private void ResetFruit() {
+        fruit.transform.localScale = fruitScale1;
+        gameObject.GetComponent<CircleCollider2D>().enabled = true;
+        
+    }
+}
---
+++ MyGame/Assets/Scripts/PlayerHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHandler : MonoBehaviour
+{
+    public float health = 100f;
+    private Vector2 spawnPoint = new Vector2(0f, 0f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0f || gameObject.transform.position.y < -8f) {
+            PlayerHasDied();
+        }
+    }
+
+    private void PlayerHasDied() {
+        // Debug.Log("PLAYE DEAD!");
+        health = 100f;
+        gameObject.transform.position = spawnPoint;
+    }
+}
