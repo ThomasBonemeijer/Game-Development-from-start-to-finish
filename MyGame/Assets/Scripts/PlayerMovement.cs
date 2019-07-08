dd7a6ad9e3ec036459e5d275e111fe67c0006c37
Thomas Bonemeijer Desktop <thomas.bonemeijer@student.ehb.be>
2019-07-08 13:40:55 +0200

    small rollback

--- MyGame/Assets/Scripts/PlayerMovement.cs
+++ MyGame/Assets/Scripts/PlayerMovement.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
+    public Animator animator;
     public Joystick joystick;
+    public Button jumpButton;
     public float runSpeed = 50f;
     float horizontalMove = 0f;
     bool jump = false;
@@ -13,7 +16,7 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        // horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
         // horizontalMove = joystick.Horizontal * runSpeed;
 
         if (joystick.Horizontal >= .2f) {
@@ -24,9 +27,21 @@ public class PlayerMovement : MonoBehaviour
             horizontalMove = 0;
         }
 
-        if (Input.GetButtonDown("Jump")) {
-            jump = true;
-        }
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        // if (Input.GetButtonDown("Jump")) {
+        //     jump = true;
+        // }
+        jumpButton.onClick.AddListener(CharacterJump);
+    }
+
+    private void CharacterJump() {
+        jump = true;
+        animator.SetBool("IsJumping", true);
+    }
+
+    public void OnLanding() {
+        animator.SetBool("IsJumping", false);
     }
 
     void FixedUpdate() {
