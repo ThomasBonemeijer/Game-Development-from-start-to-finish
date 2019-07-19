c637e114a6179edc9cd4ddd467c32bb17ce6a348
Thomas Bonemeijer Desktop <thomas.bonemeijer@student.ehb.be>
2019-07-19 16:56:16 +0200

    Added scene 2 & dynamic UI button for transitions when in contact with a travel point.

---
+++ MyGame/Assets/Scripts/TravelPointHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TravelPointHandler : MonoBehaviour
+{
+    public string sceneName;
+    public GameObject useButton;
+
+    void OnTriggerEnter2D(Collider2D col) {
+        if (col.gameObject.name == "Player") {
+            useButton.SetActive(true);
+        }else {
+            useButton.SetActive(false);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col) {
+        if (col.gameObject.name == "Player") {
+            useButton.SetActive(false);
+        }
+    }
+
+    public void ChangeScene(){
+        if (sceneName != "") {
+            SceneManager.LoadScene (sceneName);
+        }
+    }
+}
